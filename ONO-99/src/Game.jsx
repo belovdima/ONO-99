88bd2ab1e34c481927e38bb2c84a131c494c1101
Dmitry Belov <machete75111@gmail.com>
2024-08-02 19:00:29 +0300

    added people list to game component

--- ONO-99/src/Game.jsx
+++ ONO-99/src/Game.jsx
@@ -1,13 +1,27 @@
-function Game ({onNavigate}) {
+import React, { useState, useEffect } from "react";
 
-    return(
+function Game({ onNavigate }) {
+    const [people, setPeople] = useState([]);
+
+    // useEffect для загрузки данных из localStorage при монтировании компонента
+    useEffect(() => {
+        const savedPeople = JSON.parse(localStorage.getItem("people")) || [];
+        setPeople(savedPeople);
+    }, []);
+
+    return (
         <div>
             <h1>GAME</h1>
             <button type="button" onClick={() => onNavigate("mainmenu")}>HOME</button>
+            
+            <h2>Игроки</h2>
+            <ol>
+                {people.map((person, index) => (
+                    <li>{person}</li>
+                ))}
+            </ol>
         </div>
     );
 }
 
-
-
-export default Game
+export default Game;
